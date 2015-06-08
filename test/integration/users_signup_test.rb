c9705497b5763dd41cab6be62fea55272e7f2df4
benzinoanas <benzinoanas@gmail.com>
2015-06-08 12:07:00 +0100

    adding sign_up functionality

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  #Verify that after an invalid submition 
+  #the number of records in the database did not change
+  test "Invalid signup information" do
+  	get signup_path
+  	before_count = User.count
+  	post users_path, user: {name: "",
+              							email: "user@example",
+              							password: "skrt",
+              						  password_confirmation: "secret"}
+  	after_count = User.count
+
+  	assert_equal before_count, after_count
+  	assert_template 'users/new'
+  end
+
+  test "Valid signup information" do
+    get signup_path
+    before_count = User.count
+    post_via_redirect users_path, user: { name: "User Example",
+                                          email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password"}
+    after_count = User.count - 1
+
+    assert_equal before_count, after_count
+    
+    assert_template 'users/show'
+  end
+end
